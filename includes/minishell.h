abbc11b5c208d0cd503fdc554a196a0478f305ac
dantol29 <dantol29@gmail.com>
2024-01-16 16:21:36 +0100

    command path finder

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,19 @@
 /*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:02:31 by dtolmaco          #+#    #+#             */
-/*   Updated: 2024/01/16 15:03:07 by dtolmaco         ###   ########.fr       */
+/*   Updated: 2024/01/16 16:15:32 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+typedef struct s_shell
+{
+	char	*cmd_path;
+}	t_shell;
+
+#include "../libft/libft.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <readline/readline.h>
--- src/main.c
+++ src/main.c
@@ -6,17 +6,43 @@
 /*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:58:49 by dtolmaco          #+#    #+#             */
-/*   Updated: 2024/01/16 15:04:28 by dtolmaco         ###   ########.fr       */
+/*   Updated: 2024/01/16 16:20:35 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+char	*find_path(char **envp)
+{
+	while ((ft_strncmp(*envp, "PATH", 4)) != 0)
+		envp++;
+	printf("Env path: %s\n", *envp + 5);
+	return (*envp + 5);
+}
+
+char	*get_path(char **splitted_path, char *command)
+{
+	char	*tmp;
+	char	*cmd;
+
+	while (*splitted_path)
+	{
+		tmp = ft_strjoin(*splitted_path, "/");
+		cmd = ft_strjoin(tmp, command);
+		free(tmp);
+		if (access(cmd, 0) == 0)
+			return(cmd);
+		free(cmd);
+		splitted_path++;
+	}
+	return (NULL);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
+	t_shell shell;
 	(void)argv;
 	(void)argc;
-	(void)envp;
 	// check if shell is running in interactive mode
 	if (isatty(STDIN_FILENO))
 	{
@@ -25,7 +51,9 @@ int	main(int argc, char **argv, char **envp)
 		// getcwd - get the current working directory
 		getcwd(cwd, sizeof(cwd));
 		printf("%s$", cwd);
-		line = readline("~");
+		line = readline("Enter a command: ");
 		printf("You entered: %s\n", line);
+		shell.cmd_path = get_path(ft_split(find_path(envp), ':'), line);
+		printf("command path: %s\n", shell.cmd_path);
 	}
 }
