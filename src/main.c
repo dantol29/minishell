cb4bdc7e2ddd9286b9d2bcfee0bf146843e8ac4a
dantol29 <dantol29@gmail.com>
2024-01-18 12:54:05 +0100

    handle absolute path

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:58:49 by dtolmaco          #+#    #+#             */
-/*   Updated: 2024/01/18 12:16:30 by dtolmaco         ###   ########.fr       */
+/*   Updated: 2024/01/18 12:42:24 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,28 @@ void	ctrl_c(int signum)
 {
 	(void)signum;
 	write(1, "\n", 1);
+}
+
+void	parsing(char *line, t_shell *shell, char **envp)
+{
+	shell->command = ft_split(line, ' '); // store entered command (use ft_split for flags)
+	shell->cmd_path = ft_strjoin("/usr/bin/", shell->command[0]); // if commands whtout path (ls, pwd)
+	if (access(shell->cmd_path, 0) == 0)
+	{
+		shell->pid = fork();
+		if (shell->pid == 0)
+			execve(shell->cmd_path, shell->command, envp); // execute command (command path, command with flags, env variables)
+		waitpid(shell->pid, NULL, 0);
+	}
+	else if (access(shell->command[0], 0) == 0) // if absolute path (/bin/ls, ../bin/ls)
+	{
+		shell->pid = fork();
+		if (shell->pid == 0)
+			execve(shell->command[0], shell->command, envp); // execute command (command path, command with flags, env variables)
+		waitpid(shell->pid, NULL, 0);
+	}
+	else
+		printf("minishell: command not found: %s\n", shell->command[0]);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -32,17 +54,7 @@ int	main(int argc, char **argv, char **envp)
 	line = readline(" $ "); // read user input
 	while (line != NULL)
 	{
-		shell.command = ft_split(line, ' '); // store entered command (use ft_split for flags)
-		shell.cmd_path = ft_strjoin("/usr/bin/", shell.command[0]);
-		if (access(shell.cmd_path, 0) == 0) 
-		{
-			shell.pid = fork();
-			if (shell.pid == 0)
-				execve(shell.cmd_path, shell.command, envp); // execute command (command path, command with flags, env variables)
-			waitpid(shell.pid, NULL, 0);
-		}
-		else
-			printf("minishell: command not found: %s\n", shell.command[0]);
+		parsing(line, &shell, envp);
 		printf("%s@minishell %s", shell.username, shell.work_dir);
 		line = readline(" $ "); // read user input
 	}
